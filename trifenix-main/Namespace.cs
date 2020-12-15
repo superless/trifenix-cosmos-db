0802765d1b0a75e0e200b163570fc371e1560578
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-15 04:12:37 -0300

    incluyendo documentación de namespace

---
+++ trifenix-main/Namespace.cs
@@ -0,0 +1,10 @@
+
+namespace trifenix.main
+{
+    /// <summary>
+    /// Ejemplo de documentación sobre el namespace y
+    /// dar más detalle a la agrupación.
+    ///</summary>
+    internal class Namespace
+    {}
+}
