1e964638d78a2e3341abf35bfe346017d02ade29
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-17 19:05:39 -0300

    docs

--- exceptions/NonIdException.cs
+++ exceptions/NonIdException.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using trifenix.connect.entities.cosmos;
 
+
 namespace trifenix.connect.db.cosmos.exceptions {
+
+    /// <summary>
+    /// Excepción lanzada cuando no existe id.
+    /// </summary>
+    /// <typeparam name="T">Tipo de elemento que lanza el error</typeparam>
     public class NonIdException<T> : BaseException<T> where T:DocumentBase {
         public NonIdException(T docBase) : base(docBase) { }
 
         public override string Message => $"el elemento de tipo {DbObject.GetType()} no  tiene id";
     }
 
+
+    /// <summary>
+    /// Excepción base.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class BaseException<T> : Exception where T:DocumentBase {
         public BaseException(T docBase) {
             DbObject = docBase;
--- exceptions/Validation_Exception.cs
+++ exceptions/Validation_Exception.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 
 namespace trifenix.connect.db.cosmos.exceptions {
+
+    /// <summary>
+    /// Excepción de validación con una colección de los errores.
+    /// </summary>
     public class Validation_Exception : Exception {
 
         public List<string> ErrorMessages;
